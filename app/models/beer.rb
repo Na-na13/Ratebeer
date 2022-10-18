328c77b8d144fe2a0885c73cf5482082693d412d
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-10-18 20:04:01 +0300

    add ratings in beer view

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,15 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating(beer)
+        sum = 0
+        count = 0 
+        beer.ratings.each do |rating|
+            sum += rating.score
+            count += 1
+        end
+        average = sum/count
+        average.to_f
+    end
 end
