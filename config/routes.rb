34a8bf362902f39c82027a64500f0f98345fc29c
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-12-11 21:33:54 +0200

    search places with API

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,7 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
   delete 'signout', to: 'sessions#destroy'
-  #get 'kaikki_bisset', to: 'beers#index'
-  #get 'ratings', to: 'ratings#index'
-  #get 'ratings/new', to:'ratings#new'
-  #post 'ratings', to: 'ratings#create'
+  get 'places', to: 'places#index'
+  post 'places', to: 'places#search'
 
 end
