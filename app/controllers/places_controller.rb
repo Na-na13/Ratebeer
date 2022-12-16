da094efa3d6b1d9cc51e3972d1c03cdc844a49ba
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-12-16 14:11:15 +0200

    link to own pages of every beer place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,14 +1,22 @@
 class PlacesController < ApplicationController
-    def index
-    end
-
-    def search
-        @places = BeermappingApi.places_in(params[:city])
-        if @places.empty?
-          redirect_to places_path, notice: "No locations in #{params[:city]}"
-        else
-          render :index, status: 418
-        end
-      end
-
+  before_action :set_place, only: [:show]
+  def index
   end
+
+  def search
+    @places = BeermappingApi.places_in(params[:city])
+    if @places.empty?
+      redirect_to places_path, notice: "No locations in #{params[:city]}"
+    else
+      session[:city] = params[:city]
+      render :index, status: 418
+    end
+  end
+
+  def show
+  end
+
+  def set_place
+    @place_id = params[:id]
+  end
+end
