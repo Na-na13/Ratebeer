ca46f1a30843377babe8563d894440fa203cf210
Nana Reinikainen <nana.reinikainen@helsinki.fi>
2022-12-30 23:46:04 +0200

    rspec for beerlist page

---
+++ spec/features/beerlist_page_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+describe "Beerlist page" do
+    before :all do
+      Capybara.register_driver :chrome do |app|
+        Capybara::Selenium::Driver.new app, browser: :chrome,
+            options: Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+      end
+      
+      Capybara.javascript_driver = :chrome
+      WebMock.disable_net_connect!(allow_localhost: true)
+    end
+
+  before :each do
+    @brewery1 = FactoryBot.create(:brewery, name: "Koff")
+    @brewery2 = FactoryBot.create(:brewery, name: "Schlenkerla")
+    @brewery3 = FactoryBot.create(:brewery, name: "Ayinger")
+    @style1 = Style.create name: "Lager"
+    @style2 = Style.create name: "Rauchbier"
+    @style3 = Style.create name: "Weizen"
+    @beer1 = FactoryBot.create(:beer, name: "Nikolai", brewery: @brewery1, style:@style1)
+    @beer2 = FactoryBot.create(:beer, name: "Fastenbier", brewery:@brewery2, style:@style2)
+    @beer3 = FactoryBot.create(:beer, name: "Lechte Weisse", brewery:@brewery3, style:@style3)
+  end
+
+  it "shows one known beer", :js => true do
+    visit beerlist_path
+    find('table').find('tr:nth-child(1)')
+    expect(page).to have_content "Nikolai"
+  end
+
+  it "shows beers in alphabetical order by name", :js => true do
+    visit beerlist_path
+    beers = page.all('#beertable .tablerow')
+    expect(beers[0]).to have_content "Fastenbier"
+    expect(beers[1]).to have_content "Lechte Weisse"
+    expect(beers[2]).to have_content "Nikolai"
+  end
+
+  it "when click 'style' shows beers in alphabetical order by style", :js => true do
+    visit beerlist_path
+    find('#style').click
+    beers = page.all('#beertable .tablerow')
+    expect(beers[0]).to have_content "Nikolai"
+    expect(beers[1]).to have_content "Fastenbier"
+    expect(beers[2]).to have_content "Lechte Weisse"
+  end
+
+  it "when click 'brewery' shows beers in alphabetical order by style", :js => true do
+    visit beerlist_path
+    find('#brewery').click
+    beers = page.all('#beertable .tablerow')
+    expect(beers[0]).to have_content "Lechte Weisse"
+    expect(beers[1]).to have_content "Nikolai"
+    expect(beers[2]).to have_content "Fastenbier"
+  end
+
+end
